92faa5588d398164741c92f54dbe21dfad1817cf
Soroush Kami <soroushkami10@gmail.com>
2023-12-18 20:27:02 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by soroush kami on 2023-12-18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
